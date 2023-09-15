0472cb4c7bb8412b42e54077e27f90ff8ac38ebd
ronaldoapf <ronaldo.alves.1997@gmail.com>
2023-09-15 14:31:25 -0300

    final sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Header } from "./components/Header"
+import styles from './App.module.css'
 import './global.css'
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   return (
     <>
       <Header />
       
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </>
   )
 }
