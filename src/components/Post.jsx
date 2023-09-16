26ec496bf6c7a0272cd880b947b4a4189a42e247
ronaldoapf <ronaldo.alves.1997@gmail.com>
2023-09-16 01:20:59 -0300

    create Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,38 @@
+import styles from './Post.module.css'
+
 export function Post() {
-  return <p>Post</p>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/ronaldoapf.png" />
+          <div className={styles.authorInfo}>
+            <strong>Ronaldo Alves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="15 de setembro de 2023" dateTime='2023-09-15'>Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href='#'>jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>  
+        </p>
+      </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder='Deixe um comentário'/>
+
+        <footer>
+          <button type="submit">
+            Publicar
+          </button>
+        </footer>
+      </form>
+    </article>
+  )
 }
